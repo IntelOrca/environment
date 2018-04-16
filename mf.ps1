bf9a824a243d9e72401a563125ca4e0bd9b2d1f3
Ted John <ted@brambles.org>
2018-04-16 13:19:21 +0100

    [mf] Add jd to paths

--- mf.ps1
+++ mf.ps1
@@ -10,5 +10,6 @@ $env:MFSNK="$devbase\svn\vside\misc\win\snkeys\MicroFocus.snk"
 
 $env:PATH = "$env:NUNIT_HOME\bin\net-2.0;$env:PATH"
 $env:PATH = "$devbase\apps\ilspy;$env:PATH"
+$env:PATH = "$devbase\apps\jd;$env:PATH"
 $env:PATH = "$devbase\apps\nuget;$env:PATH"
 $env:PATH = "$devbase\apps\sysinternals;$env:PATH"
