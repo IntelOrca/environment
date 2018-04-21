2bfca98c0080e91004a5f33ce72756ead9683a01
Ted John <ted@brambles.org>
2018-04-21 19:22:34 +0100

    Update Setup-VSPrompt to find any SKU

--- include/Setup-VSPrompt.ps1
+++ include/Setup-VSPrompt.ps1
@@ -6,7 +6,25 @@ function Setup-VSPrompt
         [string]$Architecture
     )
 
-    $command = '"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"'
+    # Find VS
+    $skus = @("Enterprise", "Professional", "Community")
+    $basePath = "C:\Program Files (x86)\Microsoft Visual Studio\2017"
+    foreach ($sku in $skus)
+    {
+        $potentialPath = "$basePath\$sku"
+        if (Test-Path $potentialPath)
+        {
+            $foundPath = $potentialPath
+            break
+        }
+    }
+
+    if (-not $foundPath)
+    {
+        throw "VS 2017 could not be found"
+    }
+
+    $command = """$foundPath\Common7\Tools\VsDevCmd.bat"""
     if ($Architecture)
     {
         $command += " -arch=$Architecture"
