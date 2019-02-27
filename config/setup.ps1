caba1f92cf104b1a8647258d4768a0dbc27e7bcb
Ted John <ted@brambles.org>
2019-02-27 11:03:03 +0000

    Improve setup config script

--- config/setup.ps1
+++ config/setup.ps1
@@ -1,4 +1,12 @@
+function linkup
+{
+    param([String]$src, [String]$dst)
+
+    cmd /c "if exist ""$src"" (del ""$src"")"
+    mklink $src $dst
+}
+
 # Setup symbolic links
-mklink $home\.gitconfig (Join-Path $PSScriptRoot .gitconfig)
-mklink $home\AppData\Roaming\Code\User\settings.json (Join-Path $PSScriptRoot vscode.json)
-mklink $home\AppData\Roaming\Code\User\keybindings.json (Join-Path $PSScriptRoot vscode.keybindings.json)
+linkup $home\.gitconfig (Join-Path $PSScriptRoot .gitconfig)
+linkup $home\AppData\Roaming\Code\User\settings.json (Join-Path $PSScriptRoot vscode.json)
+linkup $home\AppData\Roaming\Code\User\keybindings.json (Join-Path $PSScriptRoot vscode.keybindings.json)
