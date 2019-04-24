927576a17d5dcaedf7d3de24f4c250f940fc6ac7
Ted John <ted@brambles.org>
2019-04-24 10:47:36 +0100

    Update JDK version

--- profiles/mf/mf.ps1
+++ profiles/mf/mf.ps1
@@ -4,7 +4,7 @@ $env:DEVBASE = "G:"
 $devbase = $env:DEVBASE
 $env:ANT_HOME = "$devbase\apps\ant"
 $env:ECLIPSE_HOME = "$devbase\apps\eclipse"
-$env:JAVA_HOME = "$devbase\apps\java\jdk1.8.0_92"
+$env:JAVA_HOME = "$devbase\apps\java\jdk8u212-b03"
 $env:NUNIT_HOME = "$devbase\apps\nunit"
 
 $env:SDKDIR = "$devbase\sdk"
